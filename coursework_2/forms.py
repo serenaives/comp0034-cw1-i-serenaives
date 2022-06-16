4098cc1287f032624e7fd3f9aa07aebd7516f10e
serenaives <serena.ives.20@ucl.ac.uk>
2022-06-16 14:27:23 +0100

    Revert "adjustments to styling"
    This reverts commit 90e39d828a5d8d8c33239e99fafc3ed80a9c42b6.

--- coursework_2/forms.py
+++ coursework_2/forms.py
@@ -1,5 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField, RadioField
+from wtforms import BooleanField
+from wtforms import PasswordField
+from wtforms import StringField
+from wtforms import SubmitField
 from wtforms.validators import DataRequired
 
 
@@ -14,8 +17,3 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Register')
-
-
-class QuestionForm(FlaskForm):
-    options = RadioField('Options: ', validators=[DataRequired()], default=1)
-    submit = SubmitField('Next')
--- coursework_2/webapp.py
+++ coursework_2/webapp.py
@@ -68,15 +68,16 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@server_bp.route('/quiz/home/')
+@server_bp.route('/quiz_home/')
 def quiz_home():
     return render_template("quiz_home.html", title='Quiz Home')
 
 
-@server_bp.route('/quiz/play/', methods=['GET', 'POST'])
+@server_bp.route('/quiz_play/', methods=['GET', 'POST'])
 @login_required
 def quiz_play():
-    return render_template('quiz_play.html', title='Quiz Play')
+    if current_user.is_authenticated:
+        return render_template("quiz_play.html", title='Quiz Play')
 
 
 @server_bp.route('/leaderboard_home/')
